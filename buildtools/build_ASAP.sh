643ac2cc941a43272eea38a71c9b3444271cf882
Péter Bándi <peter.bandi@gmail.com>
2023-08-23 22:17:48 +0200

    Make the multiresolutionimageinterface return `None` in case of OpenSlide error.

--- buildtools/build_ASAP.sh
+++ buildtools/build_ASAP.sh
@@ -20,7 +20,7 @@ ubuntu_version=$(grep 'DISTRIB_RELEASE' /etc/lsb-release | cut -d'=' -f2)
 ubuntu_version_no_dots=$(echo ${ubuntu_version} | tr -d ".")
 echo "Building ASAP with Python ${python_ver}; building GUI = ${build_gui}; on Ubuntu ${ubuntu_version}"
 if [ "${build_gui}" = "true" ] ; then \
-        cmake ../src -DPugiXML_INCLUDE_DIR=/root/pugixml-1.9/src/ -DOPENSLIDE_INCLUDE_DIR=/usr/include/openslide \
+        cmake ../src -DOPENSLIDE_INCLUDE_DIR=/usr/include/openslide \
                     -DWRAP_MULTIRESOLUTIONIMAGEINTERFACE_PYTHON=TRUE -DCMAKE_INSTALL_PREFIX=/root/install \
                     -DBUILD_ASAP=TRUE -DBUILD_EXECUTABLES=TRUE -DBUILD_IMAGEPROCESSING=TRUE -DBUILD_MULTIRESOLUTIONIMAGEINTERFACE_VSI_SUPPORT=TRUE -DCMAKE_BUILD_TYPE=Release \
                     -DDCMTKJPEG_INCLUDE_DIR=/root -DDCMTKJPEG_LIBRARY=/usr/lib/libijg8.so -DUNITTEST_INCLUDE_DIR=/usr/include/UnitTest++ \
@@ -33,7 +33,7 @@ if [ "${build_gui}" = "true" ] ; then \
                     -DPYTHON_NUMPY_INCLUDE_DIR=/root/miniconda3/envs/build_python${python_ver}/lib/python${python_ver}/site-packages/numpy/core/include \
     ; else \
         echo "Skipping GUI..."
-        cmake ../src -DPugiXML_INCLUDE_DIR=/root/pugixml-1.9/src/ -DOPENSLIDE_INCLUDE_DIR=/usr/include/openslide \
+        cmake ../src -DOPENSLIDE_INCLUDE_DIR=/usr/include/openslide \
                  -DWRAP_MULTIRESOLUTIONIMAGEINTERFACE_PYTHON=TRUE -DCMAKE_INSTALL_PREFIX=/root/install \
                  -DBUILD_MULTIRESOLUTIONIMAGEINTERFACE_VSI_SUPPORT=TRUE -DCMAKE_BUILD_TYPE=Release \
                  -DDCMTKJPEG_INCLUDE_DIR=/root -DDCMTKJPEG_LIBRARY=/usr/lib/libijg8.so -DPACKAGE_ON_INSTALL=TRUE \
